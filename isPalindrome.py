02b811a64cfb6852859551a39c594a40c6bf159c
Mike <36168675+Mike76b@users.noreply.github.com>
2018-08-29 17:59:59 +0200

    Update isPalindrome.py

--- isPalindrome.py
+++ isPalindrome.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Apr  9 12:07:38 2018
+Created on Agu 29, 2018
 
 @author: Mike
 """
 
-
 def isPalindrome(aString):
     '''
-    aString: a string
+    input of the function, aString: a string
+    returning value, boolean, whether is or is not a palindrome
     '''
     # character cleaning.
     cleaning = [",", ".", " ", "?", "!", ":", ";"]
@@ -21,24 +21,13 @@ def isPalindrome(aString):
     s1 = set(result[: (len(result)//2 + 2)])
     s2 = set(result[(len(result)//2) :])
     
-    #returning
-    return s1 == s2#, result, s1, s2, len(result) 
+    # returning
+    return s1 == s2
+    # if a deeper look is wanted, uncomment the line 27 and comment the line 25
+    #return s1 == s2, result, s1, s2, len(result)
 
 
 
 print(isPalindrome("A man, a plan, a canal: Panama!"))
-        
-
-
-
-
-
-
-
-
-
-
-
-
 
 
